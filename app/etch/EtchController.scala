226f853ea68a7857f5cdb05062b0daa608e02159
Kurtis Melby <kurtome@gmail.com>
2015-02-01 15:28:03 -0500

    updating backfill

--- app/etch/EtchController.scala
+++ app/etch/EtchController.scala
@@ -39,4 +39,5 @@ object EtchController extends Controller {
     }
   }
 
+  S3Backfill.run()
 }
--- app/etch/S3Backfill.scala
+++ app/etch/S3Backfill.scala
@@ -1,17 +1,20 @@
 package etch
 
-import org.slf4j.LoggerFactory
 import scala.concurrent.ExecutionContext.Implicits.global
+
+import org.slf4j.LoggerFactory
 
 object S3Backfill {
 
   val logger = LoggerFactory.getLogger(getClass)
 
-  EtchDao.notBackfilled().foreach { etch =>
-    EtchImageDao.saveEtch(etch) map { _ =>
-      val updated = etch.copy(s3Backfill = true)
-      EtchDao.upsertEtchE6(updated)
-      logger.info(s"backfilled ${etch.latitudeE6},${etch.longitudeE6}")
+  def run(): Unit = {
+    EtchDao.notBackfilled().foreach { etch =>
+      EtchImageDao.saveEtch(etch) map { _ =>
+        val updated = etch.copy(s3Backfill = true)
+        EtchDao.upsertEtchE6(updated)
+        logger.info(s"backfilled ${etch.latitudeE6},${etch.longitudeE6}")
+      }
     }
   }
 
